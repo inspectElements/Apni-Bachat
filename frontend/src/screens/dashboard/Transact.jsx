275762f85644102c2f9dade9328bcddb4668fcc5
aryan <aaryan.p114@gmail.com>
2023-03-04 23:04:02 +0530

    fix: typo

--- frontend/src/screens/dashboard/Transact.jsx
+++ frontend/src/screens/dashboard/Transact.jsx
@@ -10,7 +10,6 @@ import { useNavigate } from "react-router-dom";
 import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 import { db } from "../../configs/firebase";
 import { useAuth } from "@arcana/auth-react";
-import { useNavigate } from "react-router-dom";
 
 const Card = (props) => {
   return (
@@ -57,7 +56,6 @@ const Transact = () => {
   const [depMoney, setDepMoney] = useState(false);
   const [withMoney, setWithMoney] = useState(false);
   const [data, setData] = useState();
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (!auth.user) return;
@@ -120,7 +118,6 @@ const Transact = () => {
     setLoading(false);
     navigate(0);
   };
-  console.log(data);
   return (
     <>
       {loading && (
@@ -136,7 +133,7 @@ const Transact = () => {
               fill="#000"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 448 512"
-              onClick={() => navigate("/dashboard")}
+              onClick={() => window.history.back()}
             >
               <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
             </svg>
