e1bc8e160652f2f294fdc213f3311d6e9c2128ca
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-03-04 21:32:30 +0530

    feat: onboard user contract interaction

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,4 @@
-export const credibilityScoreConractAddress = "";
-export const apniBachatConractAddress = "";
+export const credibilityScoreConractAddress =
+  "0xC56Dbaf70938d640a9C718Eef5BD9aA730d61Cc5";
+export const apniBachatConractAddress =
+  "0x8d229E00544F76550D730180B658303f2A4f21c0";
