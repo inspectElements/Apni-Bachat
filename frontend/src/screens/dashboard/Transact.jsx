d150039acdc1a4947d002437e94f94ff79289d70
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-03-05 00:08:27 +0530

    feat: multilingual

--- frontend/src/screens/dashboard/Transact.jsx
+++ frontend/src/screens/dashboard/Transact.jsx
@@ -135,10 +135,6 @@ const Transact = () => {
   const withdraw = async () => {
     if (!withMoney) return;
     setLoading(true);
-
-    const amountInWei = utils.parseEther(withMoney.toString());
-    console.log("amountInWei", amountInWei);
-
     let docRef = await getDocs(collection(db, "user"));
     let r = {};
     docRef.forEach((doc) => {
@@ -184,7 +180,7 @@ const Transact = () => {
               fill="#000"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 448 512"
-              onClick={() => navigate("/dashboard")}
+              onClick={() => window.history.back()}
             >
               <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
             </svg>
