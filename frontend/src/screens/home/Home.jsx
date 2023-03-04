c37184f14c085046a2cc335097ae549bb564c629
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-03-04 15:42:56 +0530

    feat: home page done

--- frontend/src/screens/home/Home.jsx
+++ frontend/src/screens/home/Home.jsx
@@ -1,41 +1,102 @@
 import React from "react";
-import { Typography, Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { fontFamily, padding } from "@mui/system";
+import { Typography, Button, Paper } from "@mui/material";
 
-
+const Card = (props) => {
+    return (
+      <>
+        <Paper
+          elevation={3}
+          sx={{
+            width: "300px",
+            height: "75px",
+            background: "linear-gradient(91.47deg, rgba(201, 72, 247, 0.39) 0.58%, rgba(143, 0, 167, 0.39) 95.65%)",
+            border: "3px solid #fff",
+            borderRadius: "13px",
+          }}
+        >
+          <div className="flex justify-between items-center">
+            <Typography
+              variant="h4"
+              component="h2"
+              color="primary.contrastText"
+              sx={{
+                fontSize: "1.25rem",
+                color: "#000",
+                textAlign: "left",
+                pt: 2.5,
+                pl: 2,
+                fontFamily: "Poppins, sans-serif",
+              }}
+            >
+              {props.title}
+            </Typography>
+            <svg
+              style={{
+                marginTop: "1.25rem",
+                marginRight: 20,
+              }}
+              className="w-7 rotate-180"
+              fill="#fff"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+            </svg>
+          </div>
+        </Paper>
+      </>
+    );
+  };
 
 const Home = () => {
-  const navigate = useNavigate();
   return (
     <>
-    <div className="">
-      <Typography variant="h4" component="h1" gutterBottom
-      sx = {{
-        color:"blue",
-        // padding: "10px",
-        fontWeight: "bold",
-        fontFamily: "monospace",
-        margin:"auto",
-      }}
-      >
-        apni bachat
-      </Typography>
+      <div className="container pt-10 h-[100vh]">
+        <Typography
+          variant="h3"
+          component="h2"
+          color="primary.contrastText"
+          sx={{
+            fontWeight: "bold",
+            textAlign: "center",
+            mt: "35vh",
+            mb: 2.5,
+            textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+            fontFamily: "Poppins, sans-serif",
+            letterSpacing: "0.1rem",
+          }}
+        >
+          अपनी Bachat
+        </Typography>
+        <Typography
+          variant="h6"
+          color="primary.contrastText"
+          sx={{
+            fontFamily: "Poppins, sans-serif",
+            fontSize: "0.9rem",
+            fontStyle: "italic",
+            letterSpacing: "0.1rem",
+            textAlign: "center",
+            textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+            mb: 15,
+          }}
+        >
+          "Reliable and Trustworthy"
+        </Typography>
 
-      <Button variant="contained">
-        <Typography variant="h6" component="h1" >
-          Trust Score
-        </Typography>
-      </Button>
-      <Button variant="outlined">
-        <Typography variant="h6" component="h1" >
-          Login Khata
-        </Typography>
-      </Button>
-    </div>
+        <div className="flex flex-col sm:flex-row justify-center items-center gap-5 mt-10">
+        <Card
+                title="My Khata"
+                route="route"
+              />
+          <Card
+                title="My Score"
+                route="route"
+              />
+        </div>
+      </div>
     </>
   );
-
-}
+};
 
 export default Home;
