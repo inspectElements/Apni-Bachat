d664084e4a529171e7ef197eb5d15f7525fd9b37
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-03-04 16:41:32 +0530

    feat: update page back button

--- frontend/src/screens/account/Update.jsx
+++ frontend/src/screens/account/Update.jsx
@@ -6,7 +6,18 @@ const Update = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div className="my-16">
+      <div className="mt-20 mb-14">
+      <div className="absolute inset-0 mt-5 ml-5">
+      <svg
+            className="w-7"
+            fill="#000"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            onClick={() => window.history.back()}
+          >
+            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+          </svg>
+        </div>
         <Typography
           variant="h4"
           sx={{
