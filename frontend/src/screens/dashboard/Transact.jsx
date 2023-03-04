cb14aabfa5bdea406ea045f415b419fed7c49af6
aryan <aaryan.p114@gmail.com>
2023-03-04 23:02:26 +0530

    feat: view balance

--- frontend/src/screens/dashboard/Transact.jsx
+++ frontend/src/screens/dashboard/Transact.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Typography, TextField, Paper, Button, CircularProgress } from "@mui/material";
 import {
-  collection,
-  doc,
-  getDocs,
-  updateDoc,
-} from "firebase/firestore";
+  Typography,
+  TextField,
+  Paper,
+  Button,
+  CircularProgress,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 import { db } from "../../configs/firebase";
 import { useAuth } from "@arcana/auth-react";
 
@@ -49,6 +51,7 @@ const Card = (props) => {
 
 const Transact = () => {
   const auth = useAuth();
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [depMoney, setDepMoney] = useState(false);
   const [withMoney, setWithMoney] = useState(false);
@@ -61,7 +64,8 @@ const Transact = () => {
       let tData = [];
       snapshot.forEach((doc) => {
         let temp = doc.data();
-        if (!temp.kyc_done) {
+        if (temp.uid === auth.user.address) {
+          if (!temp.kyc_done) navigate('/home');
           tData.push({ ...doc.data(), id: doc.id });
         }
       });
@@ -80,12 +84,18 @@ const Transact = () => {
     let r = {};
     docRef.forEach((doc) => {
       if (doc.data().uid === auth.user.address)
-      r = { id: doc.id, amount: doc.data().balance };
+        r = {
+          id: doc.id,
+          amount: doc.data().balance,
+          kyc_done: doc.data().kyc_done,
+        };
     });
+    if (!r.kyc_done) return;
     await updateDoc(doc(db, "user", r.id), {
       balance: parseFloat(parseInt(r.amount) + parseFloat(depMoney)),
     });
     setLoading(false);
+    navigate(0);
   };
   const withdraw = async () => {
     if (!withMoney) return;
@@ -93,18 +103,25 @@ const Transact = () => {
     let docRef = await getDocs(collection(db, "user"));
     let r = {};
     docRef.forEach((doc) => {
-      if (doc.data().uid == auth.user.address)
-        r = { id: doc.id, amount: doc.data().balance };
+      if (doc.data().uid === auth.user.address)
+        r = {
+          id: doc.id,
+          amount: doc.data().balance,
+          kyc_done: doc.data().kyc_done,
+        };
     });
+    if (!r.kyc_done) return;
     if (parseInt(r.amount) < parseInt(withMoney)) return;
     await updateDoc(doc(db, "user", r.id), {
       balance: parseInt(parseInt(r.amount) - parseInt(withMoney)),
     });
     setLoading(false);
+    navigate(0);
   };
+  console.log(data);
   return (
     <>
-    {loading && (
+      {loading && (
         <div className="fixed top-0 left-0 w-screen h-screen bg-[#2e2e2e69] z-50 flex justify-center items-center">
           <CircularProgress />
         </div>
@@ -145,6 +162,7 @@ const Transact = () => {
           </p>
         </div>
         <div className="flex flex-col gap-5 justify-center items-center">
+          <Card title={`Balance: ${data && data[0]?.balance || 0}`} />
           <Card title="Deposit">
             <TextField
               label="Amount"
