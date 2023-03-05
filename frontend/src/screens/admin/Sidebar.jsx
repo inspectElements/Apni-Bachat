698324b1680dce5f7fe12d5a2942fcf6493461ae
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-03-05 16:29:04 +0530

    feat: update qr

--- frontend/src/screens/admin/Sidebar.jsx
+++ frontend/src/screens/admin/Sidebar.jsx
@@ -12,6 +12,7 @@ import { Link, useLocation } from "react-router-dom";
 import PersonIcon from "@mui/icons-material/Person";
 import CreditScoreIcon from "@mui/icons-material/CreditScore";
 import QRCode from "qrcode.react";
+import { apniBachatConractAddress } from "../../constants/index";
 
 export default function Sidebar() {
   const routes = [
@@ -35,36 +36,36 @@ export default function Sidebar() {
   return (
     <div style={{ flex: 3 }} className="container !shadow-xl">
       <Typography
-          variant="h4"
-          color="primary.contrastText"
-          sx={{
-            fontSize: "2.7rem",
-            fontWeight: "bold",
-            textAlign: "center",
-            mt: "10vh",
-            mb: 2.5,
-            textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
-            fontFamily: "Poppins, sans-serif",
-            letterSpacing: "0.1rem",
-          }}
-        >
-          अपनी Bachat
-        </Typography>
-        <Typography
-          variant="h6"
-          color="primary.contrastText"
-          sx={{
-            fontFamily: "Poppins, sans-serif",
-            fontSize: "0.9rem",
-            fontStyle: "italic",
-            letterSpacing: "0.1rem",
-            textAlign: "center",
-            textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
-            mb: 15,
-          }}
-        >
-          "Reliable and Trustworthy"
-        </Typography>
+        variant="h4"
+        color="primary.contrastText"
+        sx={{
+          fontSize: "2.7rem",
+          fontWeight: "bold",
+          textAlign: "center",
+          mt: "10vh",
+          mb: 2.5,
+          textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+          fontFamily: "Poppins, sans-serif",
+          letterSpacing: "0.1rem",
+        }}
+      >
+        अपनी Bachat
+      </Typography>
+      <Typography
+        variant="h6"
+        color="primary.contrastText"
+        sx={{
+          fontFamily: "Poppins, sans-serif",
+          fontSize: "0.9rem",
+          fontStyle: "italic",
+          letterSpacing: "0.1rem",
+          textAlign: "center",
+          textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+          mb: 15,
+        }}
+      >
+        "Reliable and Trustworthy"
+      </Typography>
 
       <List>
         {routes.map((r) => (
@@ -77,7 +78,6 @@ export default function Sidebar() {
                 bgcolor: isCurrentNavActive(r.path) ? "#54004C50" : "initial",
                 color: "white",
                 fontSize: "40px !important",
-
               }}
             >
               <ListItemButton sx={{ p: 2, px: 4 }}>
@@ -88,7 +88,7 @@ export default function Sidebar() {
                       xl: "30px",
                       sm: "20px",
                     },
-                    color: "white"
+                    color: "white",
                   }}
                 >
                   {r.mainIcon}
@@ -115,7 +115,9 @@ export default function Sidebar() {
             paddingTop: 14,
           }}
         >
-          <QRCode value={`https://mumbai.polygonscan.com/`} />
+          <QRCode
+            value={`https://mumbai.polygonscan.com/address/${apniBachatConractAddress}`}
+          />
           <Typography textAlign={"center"} variant="h6" color="white">
             Check Contract Transaction History
           </Typography>
